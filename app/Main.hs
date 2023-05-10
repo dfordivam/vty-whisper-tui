1015a95bf17b6006e086d932afaedec207bca02a
Divam <dfordivam@gmail.com>
2023-05-10 21:44:48 +0900

    init

---
+++ app/Main.hs
@@ -0,0 +1,60 @@
+module Main where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Fix
+import Data.Functor
+import Data.Functor.Misc
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Zipper as TZ
+import qualified Graphics.Vty as V
+import Reflex
+import Reflex.Network
+import Reflex.Vty
+
+type VtyExample t m =
+  ( MonadFix m
+  , MonadHold t m
+  , Reflex t
+  , HasInput t m
+  , HasImageWriter t m
+  , HasDisplayRegion t m
+  , HasFocus t m
+  , HasFocusReader t m
+  , HasTheme t m
+  )
+
+type Manager t m =
+  ( HasLayout t m
+  , HasFocus t m
+  )
+
+withCtrlC :: (Monad m, HasInput t m, Reflex t) => m () -> m (Event t ())
+withCtrlC f = do
+  inp <- input
+  f
+  return $ fforMaybe inp $ \case
+    V.EvKey (V.KChar 'c') [V.MCtrl] -> Just ()
+    _ -> Nothing
+
+darkTheme :: V.Attr
+darkTheme = V.Attr {
+  V.attrStyle = V.SetTo V.standout
+  , V.attrForeColor = V.SetTo V.black
+  , V.attrBackColor = V.Default
+  , V.attrURL = V.Default
+}
+
+main :: IO ()
+main = mainWidget $ withCtrlC $ do
+  initManager_ $ do
+    tabNavigation
+    a <- tile flex $ btn "POTATO"
+    tile (fixed 7) $ boxTitle (constant def) "Transcribed Text" $ do
+      outputDyn <- foldDyn (<>) "" $ mergeWith (<>)
+        [a $> "\129364"]
+      text (current outputDyn)
---
+++ release.nix
@@ -0,0 +1,49 @@
+{ reflex-platform ? import ./reflex-platform
+}:
+let
+  pkgs = (reflex-platform {}).nixpkgs;
+  supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+  inherit (pkgs) lib;
+  commonOverrides = self: super: {
+    reflex-vty = self.callHackageDirect {
+      pkg = "reflex-vty";
+      ver = "0.4.1.0";
+      sha256 = "sha256-R42Z9itpAgtTYqo/Q7doBn2HlMj7qKennuCQ4lq4w9o=";
+    } {};
+    vty = self.callHackageDirect {
+      pkg = "vty";
+      ver = "5.37";
+      sha256 = "01ppspii5x02iik9rxkf2idi6r9ngax6w3mqi6d8qj765w3cksiq";
+    } {};
+  };
+in
+  lib.genAttrs supportedSystems (system: let
+    rp = reflex-platform { inherit system; __useNewerCompiler = true; };
+    rpGhc = rp.ghc.override {
+      overrides = commonOverrides;
+    };
+    nixGhc = (import ./nixpkgs { inherit system; }).haskell.packages.ghc943.override {
+      overrides = self: super: commonOverrides self super // {
+        hlint = self.callHackageDirect {
+          pkg = "hlint";
+          ver = "3.5";
+          sha256 = "1np43k54918v54saqqgnd82ccd6225njwxpg2031asi70jam80x9";
+        } {};
+        patch = self.callHackageDirect {
+          pkg = "patch";
+          ver = "0.0.8.1";
+          sha256 = "0q5rxnyilhbnfph48fnxbclggsbbhs0pkn0kfiadm0hmfr440cgk";
+        } {};
+        reflex = self.callHackageDirect {
+          pkg = "reflex";
+          ver = "0.9.0.0";
+          sha256 = "0ibashkz3ifwxa61zg3fwamvjwv125l7jhjd74kgjbq13zpx23ib";
+        } {};
+        # Jailbroken until https://github.com/audreyt/string-qq/pull/3
+        string-qq = pkgs.haskell.lib.dontCheck super.string-qq;
+      };
+    };
+  in
+  {
+    ghc810 = rpGhc.callCabal2nix "vty-whisper" (import ./src.nix) {};
+  })
