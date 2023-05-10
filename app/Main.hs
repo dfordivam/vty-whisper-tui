b3d47d6d504d505a5f5e4e7cd990d44174ef633c
Divam <dfordivam@gmail.com>
2023-05-11 08:31:30 +0900

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,7 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import qualified Data.Text.Zipper as TZ
 import qualified Graphics.Vty as V
 import Reflex
@@ -18,6 +19,9 @@ import Reflex.Collection
 import Reflex.Network
 import Reflex.Process
 import Reflex.Vty
+import System.IO.Temp
+import System.Posix.Signals
+import System.Process hiding (createProcess)
 
 type VtyExample t m =
   ( MonadFix m
@@ -60,7 +64,7 @@ main = mainWidget $ withCtrlC $ do
       [ (V.KEnter, [])
       , (V.KChar ' ', [])
       ]
-    (active, nextEv') <- grout (fixed 4) $ row $ do
+    (active, nextEv', copyClipboardEv) <- grout (fixed 4) $ row $ do
       rec
         active <- toggle False toggleStartEv
         let startLabel = ffor active $ \case
@@ -68,22 +72,33 @@ main = mainWidget $ withCtrlC $ do
               _ -> "Start"
         toggleStartEv <- tile flex $ textButton def (current startLabel)
       nEv <- tile flex $ textButtonStatic def "Next"
-      pure (active, gate (current active) (leftmost [nEv, () <$ keyPress]))
+      copyClipboardEv <- tile flex $ textButtonStatic def "Copy to clipboard"
+      pure (active, gate (current active) (leftmost [nEv, () <$ keyPress]), copyClipboardEv)
     let stopEv = fforMaybe (updated active) $ \case
           True -> Just ()
           _ -> Nothing
     nextEv :: Event t (Int, ()) <- numberOccurrences nextEv'
     let addNEv = ((\(k, _) -> Map.singleton k (Just ())) <$> nextEv)
     dResults <- listHoldWithKey mempty addNEv $ \k _ -> do
-      ev <- tile flex $ textButtonStatic def ("Doing " <> tshow k)
-      pure (tshow k <$ ev)
+      fileName <- liftIO $ emptySystemTempFile "audio-"
+      stopRecordEv <- headE (keyboardSignal <$ leftmost [nextEv', stopEv])
+      recordProc <- createProcess (proc "./record-audio.sh" [fileName]) $ def { _processConfig_signal = stopRecordEv }
+      let copyAudioEv = _process_exit recordProc
+      copyAudioProc <- createProcess (proc "./copy-audio.sh" [fileName]) $ def
+      let transcribeAudioEv = _process_exit copyAudioProc
+      transcribeAudioProc <- createProcess (proc "./transcribe-audio.sh" [fileName]) $ def
+      let txtEv = T.decodeUtf8 <$> _process_stdout transcribeAudioProc
+      pure txtEv
     (addTxtEv, addTxtAction) <- newTriggerEvent
     networkView $ ffor dResults $ \m -> forM (Map.assocs m) $ \(k, ev) -> do
       performEvent $ ffor ev $ \txt -> liftIO (addTxtAction (k, txt))
     processedTxtx <- foldDyn (<>) mempty (uncurry Map.singleton <$> addTxtEv)
     tile flex $ boxTitle (constant def) "Transcribed Text" $ do
-      let outputDyn = ffor processedTxtx $ Map.foldlWithKey (\(pk, pt) k t -> if k == (pk + 1) then (k, pt <> t) else (pk, pt)) (-1, "")
-      text $ current (snd <$> outputDyn)
+      let outputTxt = current $ (snd <$>) $ ffor processedTxtx $ Map.foldlWithKey (\(pk, pt) k t -> if k == (pk + 1) then (k, pt <> t) else (pk, pt)) (-1, "")
+--       performEvent $ ffor (tag outputTxt copyClipboardEv) $ \t -> do
+-- processConfig_stdin
+--         liftIO $ callProcess "./copy-to-clipboard.sh"
+      text outputTxt
 
 tshow :: (Show a) => a -> Text
 tshow = T.pack . show
