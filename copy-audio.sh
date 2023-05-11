1a23b6c04d2ce21b6ef869f0a9f992912f733af6
Divam <dfordivam@gmail.com>
2023-05-11 10:18:08 +0900

    remote transcribe

--- copy-audio.sh
+++ copy-audio.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 echo "Doing copy audio : $@"
+REMOTE_DIR=/home/divam/scratch/transcribe
+scp "$@" build:$REMOTE_DIR/
--- transcribe-audio.sh
+++ transcribe-audio.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 echo "Doing transcribe audio : $@"
-/home/divam/scratch/sources/nixpkgs/result-whisper-cpp/bin/whisper-cpp -f "$@"  -t 8 -m /home/divam/scratch/whisper.cpp/ggml-small.en.bin -otxt
-cat "$@.txt"
-rm "$@" "$@.txt"
+base_name=$(basename "$@")
+REMOTE_DIR=/home/divam/scratch/transcribe
+REMOTE_FILE=$REMOTE_DIR/$base_name
+ssh build "
+  /home/divam/scratch/sources/nixpkgs/result-whisper-cpp/bin/whisper-cpp -f $REMOTE_FILE -t 8 -m /home/divam/scratch/whisper.cpp/ggml-small.en.bin -otxt
+  cat $REMOTE_FILE.txt
+  rm $REMOTE_FILE $REMOTE_FILE.txt
+"
+rm "$@"
