4a4fe93f49033af3f17c93aa82d806a7b826b1fc
Divam <dfordivam@gmail.com>
2023-05-11 09:34:20 +0900

    rm files

--- transcribe-audio.sh
+++ transcribe-audio.sh
@@ -2,3 +2,4 @@
 echo "Doing transcribe audio : $@"
 /home/divam/scratch/sources/nixpkgs/result-whisper-cpp/bin/whisper-cpp -f "$@"  -t 8 -m /home/divam/scratch/whisper.cpp/ggml-small.en.bin -otxt
 cat "$@.txt"
+rm "$@" "$@.txt"
