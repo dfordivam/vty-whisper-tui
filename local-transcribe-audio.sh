0a715027ac951ed21a6eb864ba5959e324a336c0
Divam <dfordivam@gmail.com>
2023-05-15 15:03:38 +0900

    add a local-transcribe-audio.sh script

---
+++ local-transcribe-audio.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "Doing transcribe audio : $@"
+/home/divam/scratch/sources/nixpkgs/result-whisper-cpp/bin/whisper-cpp -f "$@"  -t 8 -m /home/divam/scratch/whisper.cpp/ggml-small.en.bin -otxt > /dev/null
+cat "$@.txt"
+rm "$@" "$@.txt"
